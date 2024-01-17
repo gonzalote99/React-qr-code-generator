8e7bd67477fbdd22cd2a08ef6ff70d9b65465471
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2024-01-17 21:12:17 +0000

    Initial commit

---
+++ src/Components/Liveqr.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import QRCode from 'react-qr-code';
+import '../App.css';
+
+const Liveqr = ({qrval}) => {
+  return (
+    <div>
+    <div className='qrcode'><QRCode value={qrval} /></div>
+    </div>
+  )
+}
+
+export default Liveqr;
